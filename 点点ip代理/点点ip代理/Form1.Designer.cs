b84081673ce0dd1d2abe1546e5c4c46c9a4aa094
Majesty17 <a-o@qq.com>
2017-01-25 19:15:24 +0800

    初步完成~

--- 点点ip代理/点点ip代理/Form1.Designer.cs
+++ 点点ip代理/点点ip代理/Form1.Designer.cs
@@ -28,20 +28,328 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_testip = new System.Windows.Forms.TextBox();
+            this.textBox_testport = new System.Windows.Forms.TextBox();
+            this.textBox_testurl = new System.Windows.Forms.TextBox();
+            this.button_test = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(688, 562);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.numericUpDown1);
+            this.tabPage1.Controls.Add(this.linkLabel1);
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(680, 536);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "01-快代理";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(589, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(83, 12);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "kuaidaili.com";
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(8, 36);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(664, 494);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IP";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "PORT";
+            this.columnHeader2.Width = 40;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "匿名度";
+            this.columnHeader3.Width = 49;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "类型";
+            this.columnHeader4.Width = 38;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "位置";
+            this.columnHeader5.Width = 217;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "响应速度";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "最后验证时间";
+            this.columnHeader7.Width = 132;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(245, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "代理类型";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "国内高匿",
+            "国内普通",
+            "国外高匿",
+            "国外普通"});
+            this.comboBox1.Location = new System.Drawing.Point(315, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(680, 536);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "02-西刺免费代理";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.groupBox1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(680, 536);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "测试&设置";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button_test);
+            this.groupBox1.Controls.Add(this.textBox_testurl);
+            this.groupBox1.Controls.Add(this.textBox_testport);
+            this.groupBox1.Controls.Add(this.textBox_testip);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(29, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 195);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "测试代理";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(43, 6);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 21);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "页数";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "测试ip";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "测试网址";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 62);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "测试port";
+            // 
+            // textBox_testip
+            // 
+            this.textBox_testip.Location = new System.Drawing.Point(90, 25);
+            this.textBox_testip.Name = "textBox_testip";
+            this.textBox_testip.Size = new System.Drawing.Size(131, 21);
+            this.textBox_testip.TabIndex = 6;
+            // 
+            // textBox_testport
+            // 
+            this.textBox_testport.Location = new System.Drawing.Point(90, 59);
+            this.textBox_testport.Name = "textBox_testport";
+            this.textBox_testport.Size = new System.Drawing.Size(131, 21);
+            this.textBox_testport.TabIndex = 7;
+            // 
+            // textBox_testurl
+            // 
+            this.textBox_testurl.Location = new System.Drawing.Point(90, 94);
+            this.textBox_testurl.Name = "textBox_testurl";
+            this.textBox_testurl.Size = new System.Drawing.Size(131, 21);
+            this.textBox_testurl.TabIndex = 8;
+            this.textBox_testurl.Text = "http://www.baidu.com";
+            // 
+            // button_test
+            // 
+            this.button_test.Location = new System.Drawing.Point(27, 138);
+            this.button_test.Name = "button_test";
+            this.button_test.Size = new System.Drawing.Size(194, 34);
+            this.button_test.TabIndex = 9;
+            this.button_test.Text = "测试";
+            this.button_test.UseVisualStyleBackColor = true;
+            this.button_test.Click += new System.EventHandler(this.button_test_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(466, 495);
+            this.ClientSize = new System.Drawing.Size(688, 562);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "点点ip代理 v2.0";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_testurl;
+        private System.Windows.Forms.TextBox textBox_testport;
+        private System.Windows.Forms.TextBox textBox_testip;
+        private System.Windows.Forms.Button button_test;
     }
 }
 
--- 点点ip代理/点点ip代理/Form1.cs
+++ 点点ip代理/点点ip代理/Form1.cs
@@ -6,14 +6,132 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using HtmlAgilityPack;
 
 namespace 点点ip代理
 {
     public partial class Form1 : Form
     {
+        private System.Net.WebProxy proxy = null;
+        private String url = null;
+        private String data_str = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        //选择模式1
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            show_proxy1();
+        }
+        //翻页1
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            show_proxy1();
+        }
+        //显示代理1
+        private void show_proxy1() {
+            WebClient web = new WebClient();
+            int index = comboBox1.SelectedIndex;
+            int page = Convert.ToInt32(numericUpDown1.Value);
+            byte[] data = null;
+
+            try
+            {
+                switch (index)
+                {
+                    case 0:
+                        data = web.DownloadData("http://www.kuaidaili.com/free/inha/" + page + "/");
+                        break;
+                    case 1:
+                        data = web.DownloadData("http://www.kuaidaili.com/free/intr/" + page + "/");
+                        break;
+                    case 2:
+                        data = web.DownloadData("http://www.kuaidaili.com/free/outha/" + page + "/");
+                        break;
+                    case 3:
+                        data = web.DownloadData("http://www.kuaidaili.com/free/outtr/" + page + "/");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("数据抓取错误！请清除当前代理再试。");
+                return;
+            }
+            String data_str = System.Text.Encoding.UTF8.GetString(data);
+            listView1.Items.Clear();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(data_str);
+            HtmlNodeCollection nodes = doc.DocumentNode.SelectSingleNode("//tbody").SelectNodes("tr");// .SelectNodes("//tbody");
+            foreach (HtmlNode anode in nodes)
+            {
+                string ip = anode.SelectSingleNode("td[@data-title='IP']").InnerText;
+                string port = anode.SelectSingleNode("td[@data-title='PORT']").InnerText;
+                string hide = anode.SelectSingleNode("td[@data-title='匿名度']").InnerText;
+                string type = anode.SelectSingleNode("td[@data-title='类型']").InnerText;
+                string pos = anode.SelectSingleNode("td[@data-title='位置']").InnerText;
+                string speed = anode.SelectSingleNode("td[@data-title='响应速度']").InnerText;
+                string utime = anode.SelectSingleNode("td[@data-title='最后验证时间']").InnerText;
+
+                ListViewItem lvi = new ListViewItem(ip);
+                lvi.SubItems.Add(port);
+                lvi.SubItems.Add(hide);
+                lvi.SubItems.Add(type);
+                lvi.SubItems.Add(pos);
+                lvi.SubItems.Add(speed);
+                lvi.SubItems.Add(utime);
+                listView1.Items.Add(lvi);
+            }
+        }
+        //双击选中
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listView1.SelectedItems.Count <= 0) return;
+            String ip = listView1.SelectedItems[0].SubItems[0].Text;
+            String port = listView1.SelectedItems[0].SubItems[1].Text;
+
+            textBox_testip.Text=ip;
+            textBox_testport.Text=port;
+            tabControl1.SelectedIndex=2;
+        }
+        
+        
+        //代理测试
+        private void button_test_Click(object sender, EventArgs e)
+        {
+            String ip=textBox_testip.Text;
+            String port=textBox_testport.Text;
+            url=textBox_testurl.Text;
+            proxy=new WebProxy(ip,Convert.ToInt32(port));
+            System.Threading.Thread thd=new System.Threading.Thread(new System.Threading.ThreadStart(ThreadFunc));
+            thd.Start();
+        }
+
+        //测试进程
+        private void ThreadFunc() { 
+            WebClient web=new WebClient();
+			web.Proxy=this.proxy;
+			System.DateTime start=System.DateTime.Now;
+            byte[] data;
+            try{
+                data=web.DownloadData(this.url);
+                this.data_str=System.Text.Encoding.Default.GetString(data);
+            }
+            catch(Exception ex){
+                MessageBox.Show("此代理不可用，请更换代理或者更换网址再试。");
+                return;
+            }
+            System.DateTime end=System.DateTime.Now;
+            System.TimeSpan ts=end-start;
+            //MessageBox::Show(IPProxy::Form1::_data);
+            MessageBox.Show("延迟：" + (ts.Seconds * 1000 + ts.Milliseconds).ToString() + "毫秒");
+        }
+
     }
 }
